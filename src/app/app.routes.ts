9a6588a32778225ce0afab057c8ddbc70f99f27c
JosueSosa01 <117098057+JosueSosa01@users.noreply.github.com>
2025-05-21 19:14:01 -0600

    Avance
    Implementacion de inicio y resultados (ventanas)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ResultsComponent } from './results/results.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // redirige '/' a '/home'
+  { path: 'home', component: HomePageComponent },       // ruta para Inicio
+  { path: 'resultados', component: ResultsComponent }   // ruta para Resultados
+];
