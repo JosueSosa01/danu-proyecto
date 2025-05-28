49d385e9e6253cfe9e35812340535a053003534e
JosueSosa01 <117098057+JosueSosa01@users.noreply.github.com>
2025-05-27 20:58:56 -0600

    cambios login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NgFor } from '@angular/common'; // Necesario para *ngFor
-
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { filter } from 'rxjs/operators';
 import { NAV_LINKS } from './nav-links';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, NgFor], // <- ¡IMPORTANTE!
+  imports: [RouterModule, NgFor],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   navLinks = NAV_LINKS;
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const url = event.urlAfterRedirects;
+        this.showNavbar = !(url === '/login'); // Oculta solo si es exactamente /login
+      });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ResultsComponent } from './results/results.component';
+import { LoginComponent } from './login/login.component';
+
 
 export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'resultados', component: ResultsComponent },
+  { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' }
 ];
